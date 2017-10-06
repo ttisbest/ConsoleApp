a2e6f757c1f9d712f0edd4178b65d0e2fa4fa7de
Unknown <timoski@outlook.com>
2017-10-06 22:41:10 +0200

    start drawing

--- ToldYouSo/ToldYouSo/Figures/Figure.cs
+++ ToldYouSo/ToldYouSo/Figures/Figure.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ToldYouSo.Figures
 {
-    class figure
+    class Figure
     {
     }
 }
---
+++ ToldYouSo/ToldYouSo/Paint/draw.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToldYouSo.Paint
+{
+    class draw
+    {
+        private int corX;
+        private int corY;
+        private int color;
+
+        public draw()
+        {
+
+        }
+
+        public void set()
+        {
+
+        }
+
+        public void drawing()
+        {
+        }
+    }
+}
