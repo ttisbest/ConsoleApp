ab1ac021373012b055f040a9814a58b0153d1817
Unknown <timoski@outlook.com>
2017-10-23 20:44:28 +0200

    start of the game

---
+++ ToldYouSo/ToldYouSo/Game/Interactions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ToldYouSo
+{
+    class Interactions
+    {
+        public static void ClearScreen()
+        {
+            Console.Clear();
+        }
+        public static void StartTheStory()
+        {
+            string story =
+                "Hello, This is a story to proof Sietse wrong.." +
+                "\nbefore we can start i need you to do a couple of thinks for my.." +
+                "\nplease do al of this things after you have read them al.." +
+                "\nthe first thing is change the font size to 5.." +
+                "\nthe second thing is pressing 'WindowsKey + ArrowUp' at the same time.." +
+                "\nand than press 'Enter'.." +
+                "\nGoodluck..";
+            foreach (char letter in story)
+            {
+                Thread.Sleep(50);
+                Console.Write(letter);
+            }
+            Console.ReadLine();
+        }
+    }
+}
